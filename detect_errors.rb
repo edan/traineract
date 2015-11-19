c7663f0ba15aaf676691a4902ced989da5857fb7
edan <edan@edan.org>
2015-11-19 11:39:26 -0500

    simple ruby script to help with box file editing rename backup dir

---
+++ detect_errors.rb
@@ -0,0 +1,51 @@
+#!/usr/env/bin ruby
+
+box_file_name = ARGV[0]
+box_file = File.read(box_file_name)
+
+File.write("#{box_file_name}.bak", box_file)
+
+training_text = File.read('./training_text.txt')
+training_text = training_text.gsub(' ','').chomp.split('')
+
+character_count = training_text.count
+box_coordinates = box_file.split("\n")
+
+vertical_diff = "" 
+
+box_coordinates.each_with_index do |bc, idx|
+  vertical_diff << "#{bc.split.first} #{training_text[idx]} #{idx}\n"
+end
+
+if character_count > box_coordinates.count
+  puts vertical_diff
+  raise "more characters #{character_count} than boxes #{box_coordinates.count}!"
+elsif box_coordinates.count > character_count
+  puts vertical_diff
+  raise "more boxes #{box_coordinates.count} than characters #{character_count}!"
+end
+
+misses = {}
+
+box_coordinates.each_with_index do |bc, idx|
+  correct_char = training_text[idx]
+  bc_array = bc.split(' ')
+
+  if bc_array.first != correct_char
+    misses[idx] = {correct_char => bc}
+    bc_array[0] = correct_char
+    box_coordinates[idx] = bc_array.join(' ')
+  end
+
+end
+
+if misses.empty?
+  puts "wow your box file is correct!"
+else
+  puts "auto-corrected #{misses.count} misses:"
+  puts misses
+
+  File.write("#{box_file_name}", box_coordinates.join("\n") + "\n")
+
+  puts "rewrote #{box_file_name}"
+end
